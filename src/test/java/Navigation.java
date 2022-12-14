348198d3c0fccb3b750bb5a884915fc8c448d3b3
a.sus <sus.aleksandra@o2.pl>
2022-12-14 12:27:32 +0100

    Change name

--- src/test/java/Navigation.java
+++ src/test/java/Navigation.java
@@ -2,12 +2,12 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Drivers {
+public class Navigation {
     @Test
     public void chromeDriverProperty()
     {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\a.sus\\IdeaProjects\\SeleniumOLX\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.quit();
+        driver.navigate().to("https://www.google.pl/");
     }
 }
