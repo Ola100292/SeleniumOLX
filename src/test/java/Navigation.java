acd9d16e392c6b0de616e414e21fe1d27ad95098
a.sus <sus.aleksandra@o2.pl>
2022-12-14 13:12:20 +0100

    before and after

--- src/test/java/Navigation.java
+++ src/test/java/Navigation.java
@@ -1,13 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Navigation {
-    @Test
-    public void chromeDriverProperty()
+    WebDriver driver;
+
+    @BeforeEach
+    public void chromeDriverSetup()
     {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\a.sus\\IdeaProjects\\SeleniumOLX\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://www.google.pl/");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+    }
+    @AfterEach
+    public void driverQuit()
+    {
+        driver.quit();
+    }
+
+
+    @Test
+    public void open()
+    {
+        driver.get("https://www.olx.pl/");
     }
 }
