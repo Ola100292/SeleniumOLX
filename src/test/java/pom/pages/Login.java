ae0cd9a8d6dccfc5fc224c0454f79f6ef49f98fe
a.sus <sus.aleksandra@o2.pl>
2022-12-22 08:47:58 +0100

    finished login class

--- src/test/java/pom/pages/Login.java
+++ src/test/java/pom/pages/Login.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class Login {
     WebDriver driver;
-    @FindBy(id ="onetrust-accept-btn-handler")
+    @FindBy(id = "onetrust-accept-btn-handler")
     WebElement accCookies;
     @FindBy(css = "#topLoginLink > span")
     WebElement mainLoggingBt;
@@ -16,4 +16,25 @@ public class Login {
     WebElement password;
     @FindBy(className = "css-ohaivz-BaseStyles")
     WebElement loginBtn;
+
+    public void clickOnCookies() {
+        accCookies.click();
+    }
+
+    public void clickOnMainLoggingBt() {
+        mainLoggingBt.click();
+    }
+    public void setLogin(String strLogin)
+    {
+        login.sendKeys(strLogin);
+    }
+    public void setPassword(String strPass)
+    {
+        password.sendKeys(strPass);
+    }
+    public void clickOnLoginBt()
+    {
+        loginBtn.click();
+    }
+
 }
